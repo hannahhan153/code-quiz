febac3496cd99567d6b5708e46e493c91c8c3da8
Hannah Han <Han@Jungs-MacBook-Pro.local>
2022-01-13 10:11:15 -0800

    html file skeletion added

---
+++ script.js
@@ -0,0 +1,19 @@
+// click Start button
+
+// timer starts
+
+// present with question
+
+// answer question
+
+// present with another question
+
+// answer question incorrectly
+
+// time subtracted from clock
+
+// all questions answered OR timer reaches 0
+
+// game is over
+
+// save initials and score
